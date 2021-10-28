b47689bf955cc1cc77a4effa991ff9741a07e3e7
daniel <daniel.niedermuehlbicher@gmail.com>
2021-10-28 21:53:57 +0200

    try to use generated kubeconfig file

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
 provider "helm" {
   kubernetes {
-    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
-    host                   = var.kubernetes_cluster_endpoint
-    exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
-      command     = "aws-iam-authenticator"
-      args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
-    }
+    config_path = "./kubeconfig"
   }
 }
 
