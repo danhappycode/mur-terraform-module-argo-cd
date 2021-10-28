de74a5008d7d02e1767345fc90b9c27ca0c3938e
daniel <daniel.niedermuehlbicher@gmail.com>
2021-10-28 21:48:03 +0200

    trying new config

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-provider "kubernetes" {
-  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
-  host                   = var.kubernetes_cluster_endpoint
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws-iam-authenticator"
-    args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
-  }
-}
-
 provider "helm" {
   kubernetes {
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
