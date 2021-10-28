17b0b152ba23dde1c12941ac00f69fc7f490328e
daniel <daniel.niedermuehlbicher@gmail.com>
2021-10-28 22:22:55 +0200

    update config

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 provider "helm" {
   kubernetes {
-    config_path = "./kubeconfig"
+    host                   = var.kubernetes_cluster_endpoint
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+      command     = "aws"
+    }
   }
 }
 
